93b72255522edd256609a57f4a8c0ae1aec269be
Jeanpier <luis.mendoza@gizlocorp.com>
2021-12-17 16:59:56 -0500

    first commit

---
+++ src/main/java/com/gizlo/GreetingResource.java
@@ -0,0 +1,24 @@
+package com.gizlo;
+
+
+import org.jboss.logging.Logger;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    Logger logger;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        logger.info("this works");
+        return "Hello World";
+    }
+}
